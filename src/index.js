3cd5570a53341cde0bdea76e9b33c5252f7efdd4
Harold AO <36776278+haroldao@users.noreply.github.com>
2021-06-23 19:26:53 +0200

    Little square ok

--- src/index.js
+++ src/index.js
@@ -3,15 +3,55 @@ import "./style.scss"
 
 export default class Sketch{
     constructor(options){
+        this.container = options.dom
         this.time = 0
+
+        this.width = this.container.offsetWidth
+        this.height = this.container.offsetHeight
+
+
+        this.scene = new THREE.Scene()
+        this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height, 0.01, 10 )
+        this.camera.position.z = 1
+
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true
+        })
+        this.renderer.setSize(this.width, this.height)
+        this.container.appendChild(this.renderer.domElement)
+
+        this.addObjects()
         this.render()
     }
 
+    resize(){
+
+    }
+
+    addObjects(){
+        this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
+        this.material = new THREE.MeshNormalMaterial()
+
+        this.mesh = new THREE.Mesh(this.geometry, this.material)
+        this.scene.add(this.mesh)
+    }
+
     render() {
-        this.time+=0.05
-        // console.log(this.time)
+        this.time += 0.05
+        this.mesh.rotation.x = this.time / 2000
+        this.mesh.rotation.y = this.time / 1000
+        this.renderer.render(this.scene, this.camera)
+
         window.requestAnimationFrame(this.render.bind(this))
     }
 }
 
-new Sketch()
+new Sketch({
+    dom: document.querySelector(".container")
+})
+
+
+
+
+
+
