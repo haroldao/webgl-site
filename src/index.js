74b03b068a113f50145e98e860a71461bb46b3c8
Harold AO <36776278+haroldao@users.noreply.github.com>
2021-06-24 00:08:06 +0200

    use Class

--- src/index.js
+++ src/index.js
@@ -2,52 +2,73 @@ import * as THREE from 'three'
 import OrbitControls from 'three-orbitcontrols'
 import "./style.scss"
 
+// import * as dat from 'dat.gui'
+// const gui = new dat.GUI()
 export default class Sketch {
-    constructor(options) {
-        this.container = options.dom
-        this.time = 0
+	constructor(options) {
+		this.container = options.dom
+		this.time = 0
 
-        this.width = this.container.offsetWidth
-        this.height = this.container.offsetHeight
-        
-        this.scene = new THREE.Scene()
-        this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 0.01, 10)
-        this.camera.position.z = 1
+		this.width = this.container.offsetWidth
+		this.height = this.container.offsetHeight
 
-        this.renderer = new THREE.WebGLRenderer({
-            antialias: true
-        })
-        this.renderer.setSize(this.width, this.height)
-        this.container.appendChild(this.renderer.domElement)
+		this.scene = new THREE.Scene()
+		this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 0.01, 10)
+		this.camera.position.z = 1
 
-        this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+		this.renderer = new THREE.WebGLRenderer({
+			antialias: true
+		})
+		this.renderer.setSize(this.width, this.height)
+		this.container.appendChild(this.renderer.domElement)
 
-        this.addObjects()
-        this.render()
-    }
+		this.controls = new OrbitControls(this.camera, this.renderer.domElement)
 
-    resize() {
+		this.resize()
+		this.addObjects()
+		this.render()
+	}
 
-    }
+	resize() {
+		window.addEventListener("resize", ()=>{
+			this.width = this.container.offsetWidth
+			this.height = this.container.offsetHeight
+			this.renderer.setSize(this.width, this.height)
+			this.camera.aspect = this.width/this.height
+			this.camera.updateProjectionMatrix()
+		})
+	}
 
-    addObjects() {
-        this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
-        this.material = new THREE.MeshNormalMaterial()
+	addObjects() {
+		this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
+		this.material = new THREE.MeshNormalMaterial()
 
-        this.mesh = new THREE.Mesh(this.geometry, this.material)
-        this.scene.add(this.mesh)
-    }
+		this.material = new THREE.ShaderMaterial({
+			fragmentShader: `
+				void main(){
+					gl_FragColor = vec4(1.,0.,1.,1);
+				}
+			`,
+			vertexShader: `
+				void main(){
+					gl_Position = projectionMatrix * modelViewMatrix * vec4 (position, 1.0);
+				}
+			`,
+		})
+		this.mesh = new THREE.Mesh(this.geometry, this.material)
+		this.scene.add(this.mesh)
+	}
 
-    render() {
-        this.time += 0.05
-        this.mesh.rotation.x = this.time / 2000
-        this.mesh.rotation.y = this.time / 1000
-        this.renderer.render(this.scene, this.camera)
+	render() {
+		this.time += 0.05
+		this.mesh.rotation.x = this.time / 2000
+		this.mesh.rotation.y = this.time / 1000
+		this.renderer.render(this.scene, this.camera)
 
-        window.requestAnimationFrame(this.render.bind(this))
-    }
+		window.requestAnimationFrame(this.render.bind(this))
+	}
 }
 
 new Sketch({
-    dom: document.querySelector(".container")
+	dom: document.querySelector(".container")
 })
